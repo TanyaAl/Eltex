eebf4a4873121b76ac27177e1e9cb6e42294743e
tatiana <alks.tatiana@gmail.com>
2026-03-30 18:11:30 +0700

    addClasses

--- src/js/model.js
+++ src/js/model.js
@@ -1,35 +1,23 @@
-const existPosts = document.querySelectorAll('.card');
-const getExistsPosts = () => {
-  const initOldArticles = Array.from(existPosts).map((post, index) => {
-    const id = String(index + 1);
-    post.setAttribute('data-id', id);
-    return {
-      id,
-      title: post.querySelector('h3').textContent,
-      content: post.querySelector('.text')?.textContent || '',
-    };
-  });
-
-  return initOldArticles;
-};
+// eslint-disable-next-line import/extensions
+import Storage from './ArticleStorage.js';
 
 const state = {
-  posts: getExistsPosts(),
+  posts: Storage.get('articles'),
 };
 
 const addPostToState = (newPost) => {
-  // eslint-disable-next-line no-param-reassign
-  newPost.id = state.posts.length + 1;
   state.posts.push(newPost);
+  Storage.save('articles', state.posts);
 };
 
 const deletePostFromState = (post) => {
   const dataId = post.dataset.id;
   const newPosts = state.posts.filter((item) => dataId !== String(item.id));
   state.posts = newPosts;
+  Storage.save('articles', state.posts);
   return state;
 };
 
 export {
-  getExistsPosts, state, addPostToState, deletePostFromState,
+  state, addPostToState, deletePostFromState,
 };
