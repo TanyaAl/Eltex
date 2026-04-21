8452228587387016558ebd6b0272ee4554f20ac2
tatiana <alks.tatiana@gmail.com>
2026-04-21 22:26:01 +0700

    addNewPostsLogic

--- blog-app/src/app/ui/components/form-for-add-blog-post/form-for-add-blog-post.ts
+++ blog-app/src/app/ui/components/form-for-add-blog-post/form-for-add-blog-post.ts
@@ -1,9 +1,11 @@
+/* eslint-disable no-useless-return */
 /* eslint-disable import/prefer-default-export */
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-for-add-blog-post',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-for-add-blog-post.html',
   styleUrl: './form-for-add-blog-post.scss',
 })
@@ -11,4 +13,31 @@ export class FormForAddBlogPost {
   @Input() isOpen = false;
 
   @Output() close = new EventEmitter<void>();
+
+  @Output() save = new EventEmitter<any>();
+
+  blogPostForm = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.minLength(25)]),
+    text: new FormControl('', Validators.required),
+  });
+
+  get title() {
+    return this.blogPostForm.get('title');
+  }
+
+  get text() {
+    return this.blogPostForm.get('text');
+  }
+
+  onSubmit() {
+    if (this.blogPostForm.invalid) return;
+    this.save.emit(this.blogPostForm.value);
+    console.log(this.blogPostForm.value);
+    this.blogPostForm.reset();
+  }
+
+  onCancel() {
+    this.blogPostForm.reset();
+    this.close.emit();
+  }
 }
--- blog-app/src/app/ui/pages/blog-page/blog-page.ts
+++ blog-app/src/app/ui/pages/blog-page/blog-page.ts
@@ -5,6 +5,7 @@ import { BtnOrLink } from '../../components/btn-or-link/btn-or-link';
 import { BlogPostsContainer } from '../../components/blog-posts-container/blog-posts-container';
 import { TipButtons } from '../../components/tip-buttons/tip-buttons';
 import { FormForAddBlogPost } from '../../components/form-for-add-blog-post/form-for-add-blog-post';
+import { BlogPostType } from '../../../types/BlogPostType';
 
 @Component({
   selector: 'app-blog-page',
@@ -16,4 +17,26 @@ export class BlogPage {
   postsCount = 0;
 
   isFormOpen = false;
+
+  blogPosts: BlogPostType[] = [];
+
+  onSave(value: { title: string; text: string }) {
+    const todayDate = new Date()
+      .toLocaleDateString('ru-RU', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+      })
+      .replace('г', '')
+      .replace('.', '');
+
+    const newPost: BlogPostType = {
+      id: crypto.randomUUID(),
+      category: 'general',
+      title: value.title,
+      text: value.text,
+      date: todayDate,
+    };
+    this.blogPosts = [...this.blogPosts, newPost];
+  }
 }
